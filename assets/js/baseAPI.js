16e82c8f3ebcc3607570943124302f42fa0da286
董尧 <1142283157@qq.com>
2020-07-29 20:03:15 +0800

    提交index文件

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,22 @@ $(function () {
     //每次调用$.get(),$.post(),$.ajax()的时候,都会先调用ajaxPrefilter 这个函数
     //在这个函数中,可以拿到我们给ajax提供的配置对象
     $.ajaxPrefilter(function(options) {
-        options.url = 'http://ajax.frontend.itheima.net' + options.url
+        options.url = 'http://ajax.frontend.itheima.net' + options.url;
+        if(options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization:localStorage.getItem('token')||''
+            }
+        }
+        //全局统一挂载 complete 回调函数
+        options.complete = function(res) {
+            // 在complete 回调函数中,可以使用 res.responseJSON 拿到服务器响应回来的数据
+            if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+                //1. 强制清空token
+                localStorage.removeItem('token');
+                // 2. 强制跳转到登录页面
+                location.href = "/login.html"
+            }
+        }
     })
+   
 })
